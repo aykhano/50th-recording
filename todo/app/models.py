9d30e7aaf8f628466d47632361c8132aca421c67
Ayxan <aykhanoscar556@gmail.com>
2025-03-03 09:35:45 +0400

    second day

--- todo/app/models.py
+++ todo/app/models.py
@@ -5,26 +5,54 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=255)
     todo_list = models.ForeignKey(
-        "app.ToDoList",
+        "app.ToDolist", 
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
     )
 
     def __str__(self):
-        return f"{self.title}"
-    
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
-
+        return f"Task {self.title}"
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
 
 class ToDoList(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    label = models.CharField(max_length=32, null=True, blank=True)
+    label = models.CharField(max_length=32, blank=True)
 
     def __str__(self):
-        return f"{self.user.username}'s todo list"
+        return f"{self.user.username}'s tasks"
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import User
+
+
+# class Task(models.Model):
+#     title = models.CharField(max_length=255)
+#     todo_list = models.ForeignKey(
+#         "app.ToDoList",
+#         on_delete=models.SET_NULL,
+#         null=True,
+#         blank=True,
+#     )
+
+#     def __str__(self):
+#         return f"Task {self.title}"
+    
+#     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+#     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+
+
+# class ToDoList(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     label = models.CharField(max_length=32, null=True, blank=True)
+
+#     def __str__(self):
+#         return f"{self.user.username}'s todo list"
 
 
 
--- todo/app/views.py
+++ todo/app/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render
-from .models import Task
+from django.shortcuts import render, redirect
+from .models import Task, ToDoList
+
 
 def home_page(request):
-    tasks = Task.objects.all()
+    user = request.user
+    todo_list = ToDoList.objects.get(user=user)
+    tasks = Task.objects.filter(todo_list=todo_list)
     context = {"task_list": tasks}
     return render(request, 'home.html', context)
 
+
 def create_new_tasks(request):
+    user = request.user
+    todo_list = ToDoList.objects.get(user=user)
     task = request.POST.get("task_title")
-    Task.objects.create(title=task)
-    pass
+    Task.objects.create(
+        todo_list = todo_list,
+        title = task,
+    )
+    return redirect("http://127.0.0.1:8000/")
+
+
+# from django.shortcuts import render, redirect
+# from .models import Task, ToDoList
+
+# def home_page(request):
+#     user = request.user
+#     todo_list = ToDoList.objects.get(user=user)
+#     tasks = Task.objects.filter(todo_list=todo_list)
+#     context = {"task_list": tasks}
+#     return render(request, 'home.html', context)
+
+# def create_new_tasks(request):
+#     user=request.user
+#     todo_list = ToDoList.objects.get(user=user)
+#     task = request.POST.get("task_title")
+#     Task.objects.create(
+#         todo_list=todo_list, 
+#         title=task,
+#     )
+#     return redirect("http://127.0.0.1:8000/")
